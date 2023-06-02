df2afa89509c17a9a68f21c3f3d8c9910fdfc0eb
SnowflakeDom <135008020+SnowflakeDom@users.noreply.github.com>
2023-06-02 15:52:17 +0000

    cleaning up

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,12 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
+def get_fruityvice_data(fruit):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -31,8 +37,7 @@ try:
     if not fruit_choice:
         streamlit.error('Please select a fruit to get information')
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        fruityvice_normalized = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
